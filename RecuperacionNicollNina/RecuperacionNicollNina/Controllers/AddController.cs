7ba81629396705f3c2510987913aa7ca80a360c9
Karla Nicoll Nina Muñoz <a2016118253@estudiantes.upsa.edu.bo>
2021-04-07 23:46:24 -0400

    Se agrego Addcontroller con Get

---
+++ RecuperacionNicollNina/RecuperacionNicollNina/Controllers/AddController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RecuperacionNicollNina.Controllers
+{
+    public class AddController : ApiController
+    {
+        [HttpGet]
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
